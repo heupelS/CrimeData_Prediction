d0d521ef4ab11a93b47abdb90de0d175674dc3a9
heupelS <simeon@heupel.de>
2022-07-07 14:59:08 +0200

    everything in functions

--- models.py
+++ models.py
@@ -15,44 +15,44 @@ import datetime
 #%% Overview
 
 crimeData = pd.DataFrame(pd.read_csv(data_path))
-print("Total number of crimes in the dataset: {}".format(len(crimeData)))
+#print("Total number of crimes in the dataset: {}".format(len(crimeData)))
 
 
 #%% Cleaning
+def clean_data():
+    crimeData = crimeData.dropna(subset=['AREA','TIME.OCC','CrmCd.Desc'])
+    crimeData['date2'] = pd.to_datetime((crimeData['DATE.OCC']))
+    crimeData['Year'] = crimeData['date2'].dt.year
+    crimeData['Month'] = crimeData['date2'].dt.month
+    crimeData['Day'] = crimeData['date2'].dt.day
 
-crimeData = crimeData.dropna(subset=['AREA','TIME.OCC','CrmCd.Desc'])
-crimeData['date2'] = pd.to_datetime((crimeData['DATE.OCC']))
-crimeData['Year'] = crimeData['date2'].dt.year
-crimeData['Month'] = crimeData['date2'].dt.month
-crimeData['Day'] = crimeData['date2'].dt.day
+    time2 = []
 
-time2 = []
+    #convert TIME.OCC into readable time format
+    time1 = list(crimeData['TIME.OCC'].astype(str))
+    for t in range(len(time1)):
+        if len(time1[t]) == 4:
+            continue
+        elif len(time1[t]) == 3:
+            time1[t] = '0' + time1[t]
+        elif len(time1[t]) == 2:
+            time1[t] = '00' + time1[t]
+        else:
+            time1[t] = '000' + time1[t]
+        
 
-#convert TIME.OCC into readable time format
-time1 = list(crimeData['TIME.OCC'].astype(str))
-for t in range(len(time1)):
-    if len(time1[t]) == 4:
-        continue
-    elif len(time1[t]) == 3:
-        time1[t] = '0' + time1[t]
-    elif len(time1[t]) == 2:
-        time1[t] = '00' + time1[t]
-    else:
-        time1[t] = '000' + time1[t]
-    
+    crimeData['time2'] = time1
+    crimeData['time2'] = pd.to_datetime(crimeData['time2'],format='%H%M')
+    crimeData['Hour'] = crimeData['time2'].dt.hour
+    crimeData['Minute'] = crimeData['time2'].dt.minute
 
-crimeData['time2'] = time1
-crimeData['time2'] = pd.to_datetime(crimeData['time2'],format='%H%M')
-crimeData['Hour'] = crimeData['time2'].dt.hour
-crimeData['Minute'] = crimeData['time2'].dt.minute
+    crimeData = crimeData.drop(['date2'], axis= 1)
+    crimeData = crimeData.drop(['time2'], axis= 1)
 
-crimeData = crimeData.drop(['date2'], axis= 1)
-crimeData = crimeData.drop(['time2'], axis= 1)
+    crimeData.head(5)
 
-crimeData.head(5)
-
-print("Total number of crimes in the dataset: {}".format(len(crimeData)))
-crimeData.head()
+    print("Total number of crimes in the dataset: {}".format(len(crimeData)))
+    crimeData.head()
 
 #%% features and targets
 
@@ -63,19 +63,24 @@ targets = ['AREA.NAME']
 crimeData_train, crimeData_test = train_test_split(crimeData, test_size=0.33, random_state=10)
 
 #%% Decision tree modeling
-
-clf = tree.DecisionTreeClassifier(max_depth=5)
-cl_fit = clf.fit(crimeData_train[features], crimeData_train['AREA.NAME'])
-print("Model Accuracy:")
-print(cl_fit.score(crimeData_test[features],crimeData_test['AREA.NAME']))
+def decision_tree():
+    clf = tree.DecisionTreeClassifier(max_depth=5)
+    cl_fit = clf.fit(crimeData_train[features], crimeData_train['AREA.NAME'])
+    print("Model Accuracy:")
+    print(cl_fit.score(crimeData_test[features],crimeData_test['AREA.NAME']))
+    return cl_fit
 
 #%% visualization
-listOfClassNames = list(set(crimeData['AREA.NAME']))
-dot_data = tree.export_graphviz(cl_fit, out_file=None, feature_names=features, class_names= listOfClassNames, filled=True, rounded=True, special_characters=True)
-graph = pydotplus.graph_from_dot_data(dot_data)
+def visualize():
+    listOfClassNames = list(set(crimeData['AREA.NAME']))
+    dot_data = tree.export_graphviz(cl_fit, out_file=None, feature_names=features, class_names= listOfClassNames, filled=True, rounded=True, special_characters=True)
+    graph = pydotplus.graph_from_dot_data(dot_data)
 
-graph.write_pdf("decTree_iris.pdf")
+    graph.write_pdf("decTree_iris.pdf")
 
+##%
+def get_all_crimetypes():
+    return crimeData['CrmCd.Desc'].unique()
 # %%
 if __name__ == "__main__":
 
